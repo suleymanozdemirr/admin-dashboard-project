95b4e3ece7ed3a3d61717285f467e4fede4d173d
suleymanozdemirr <suleyman07ozdemir@gmail.com>
2024-02-25 22:03:43 +0300

    final

--- src/components/dashboard/Transactions.jsx
+++
@@ -1,7 +0,0 @@
-
-
-const Transactions = () => {
-  return <div></div>
-}
-
-export default Transactions
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import Wrapper from "../components/dashboard/Wrapper"
+import React from 'react'
+import Wrapper from '../components/dashboard/Wrapper'
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-[25px] px-[25px]">
       <Wrapper />
     </div>
   )
